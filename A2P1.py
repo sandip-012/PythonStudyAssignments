075768f05c319050c28590d47ac3a59a73c5041c
sandip-012 <sandighosh489@gmail.com>
2025-05-11 14:11:07 +0530

    Rename and 2nd assignment

---
+++ A2P1.py
@@ -0,0 +1,6 @@
+#Task 1: Check if a Number is Even or Odd
+#Input
+i=int(input("Enter Your no: "))
+if i%2==0:
+    print("Entered number" , i ,"is a even number")
+else:print("Entered number" , i ,"is a odd number")
---
+++ A2P2.py
@@ -0,0 +1,5 @@
+#Task 2: Sum of Integers from 1 to 50 Using a Loop
+n=0
+for i in range(1, 51):
+    n += i
+print("The sum of numbers from 1 to 50 is:", n)
